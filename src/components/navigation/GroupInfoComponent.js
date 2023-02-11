3aaaa5bed96ef1e71b1371213cb10f829cea5ce3
Daniil848 <ladygindaniil848@gmail.com>
2023-02-11 23:03:12 +0300

    group info done

--- src/components/navigation/GroupInfoComponent.js
+++ src/components/navigation/GroupInfoComponent.js
@@ -1,53 +1,75 @@
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import { Typography, Stack} from "@mui/material";
 
 const GroupInfoComponent = () => {
   const state = useSelector(state => state.adminReducer);
+  const [group , setGroup] = useState();
+  const { groupID } = useParams();
   const formAnimationGroupInfo = (state.groupInfo) ? "form--animation1" : "";
   const styles = {
     fontWeight : 500,
     fontSize: 16,
     my : 1,
   }
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`http://localhost:3002/group/${groupID}`);
+      const data = await response.json();
+      setGroup(data);
+      console.log(data);
+    };
+    fetchData();
+  },[groupID]);
+  console.log(groupID)
 
   if (state.groupInfo) {
     return (
       <div className={`form ${formAnimationGroupInfo}`}>
+
         <Typography
         variant="caption"
         sx = {{ fontWeight : 500, fontSize: 18, my : 1.5}}
         >Группа:</Typography>
+
         <Stack sx={{width : 1}}>
-          {state.group.name !== "" && <Typography
+          <Typography
             variant="caption"
             sx = {styles}
-          >Название: {state.group.name}</Typography>}
-          {state.date.budget !== "" && <Typography
+          >Название: { groupID ? group.group.name : state.group.name }</Typography>
+
+          <Typography
             variant="caption"
             sx = {styles}
-          >Бюджет: {state.date.budget}₽</Typography>}
-          {state.date.exchange !== "" && <Typography
+          >Бюджет: {groupID ? group.date.budget : state.date.budget}₽</Typography>
+
+          <Typography
             variant="caption"
             sx = {styles}
-          >Обмен подарками: {state.date.exchange}</Typography>}
+          >Обмен подарками: {groupID ? group.date.exchange : state.date.exchange}</Typography>
         </Stack>
+
         <Typography
         variant="caption"
         sx = {{ fontWeight : 500, fontSize: 18, my : 1.5}}
         >Ваши данные:</Typography>
+
         <Stack sx={{width : 1}}>
-          {state.user.name !== "" && <Typography
+          <Typography
             variant="caption"
             sx = {styles}
-          >Имя: {state.user.name}</Typography>}
-          {state.user.email !== "" && <Typography
+          >Имя: {state.user.name}</Typography>
+
+          <Typography
             variant="caption"
             sx = {styles}
-          >Почта: {state.user.email}</Typography>}
-          {state.gift.wiches === "" && <Typography
+          >Почта: {state.user.email}</Typography>
+
+          <Typography
             variant="caption"
             sx = {{ fontWeight : 500, fontSize: 16, my : 1}}
-          >Пожеланияя к подарку: {state.gift.wiches}</Typography>}
+          >Пожеланияя к подарку: {state.gift.wiches}</Typography>
         </Stack>
       </div>
     );
