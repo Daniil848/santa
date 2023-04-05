c69add2b0535a603ce34b88fb5c3496f954668c7
Daniil848 <ladygindaniil848@gmail.com>
2023-04-05 13:51:15 +0300

    BrowserRouter basename

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import App from './App';
 const store = configureStore({reducer}, applyMiddleware(thunk));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Provider store={store}>
       <App/>
     </Provider>
